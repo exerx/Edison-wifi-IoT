b2991221cabc06ad225980020ab79c292f2a7d00
Ever <exer.die@gmail.com>
2015-11-09 11:16:35 -0600

    Actualización a español

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-/*jslint node:true, vars:true, bitwise:true, unparam:true */
-/*jshint unused:true */
-// Leave the above lines for propper jshinting
-//Type Node.js Here :)
 
-//var mraa = require('mraa'); //require mraa
-//var LM35 = new mraa.Aio(0); //setup access analog input Analog pin #0 (A0)
 var m = require('mraa'); //require mraa
-var analogPin1 = new m.Aio(1); //setup access analog inpuput pin 0
-//var analogValueFloat = analogPin1.readFloat(); //read the pin value as a float
+var analogPin1 = new m.Aio(1); 
 var analogValue = analogPin1.read(); 
 
 
@@ -17,18 +10,18 @@ var groveSensor = require('jsupm_grove');
 var temp = new groveSensor.GroveTemp(0);
 
 
-//Content for enable IoT
+//Datos necesarios para e IoT
 var dgram = require('dgram');
 var client = dgram.createSocket('udp4');
 
-// UDP Options
+// UDP Opciones
 var options = {
     host : '127.0.0.1',
     port : 41234
 };
 
 var countmsg =0;
-//Intel Enable Iot Send Observation Info.
+//Observaion IoT
 function sendObservation(name, value, on)
 {
     var msg = JSON.stringify
@@ -43,15 +36,15 @@ function sendObservation(name, value, on)
     client.send(sentMsg, 0, sentMsg.length, options.port, options.host);
 }
 
-measureTemperature();
+medirTemperatura();
 medirHumedad();
 
-function measureTemperature()
+function medirTemperatura()
 {
     var Temperatura = temp.value();
     sendObservation("temp", Temperatura, new Date().getTime());
     console.log(Temperatura);
-    setTimeout(measureTemperature, 10000);
+    setTimeout(medirTemperatura, 10000);
 }
     
 function medirHumedad()
