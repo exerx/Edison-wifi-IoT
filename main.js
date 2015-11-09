a7b8004727672fcf02e6900b35aed8bf96afae79
Ever <exer.die@gmail.com>
2015-11-09 00:09:00 -0600

    IoT Update

--- main.js
+++ main.js
@@ -1,62 +1,63 @@
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+// Leave the above lines for propper jshinting
+//Type Node.js Here :)
+
+//var mraa = require('mraa'); //require mraa
+//var LM35 = new mraa.Aio(0); //setup access analog input Analog pin #0 (A0)
 var m = require('mraa'); //require mraa
+var analogPin1 = new m.Aio(1); //setup access analog inpuput pin 0
+//var analogValueFloat = analogPin1.readFloat(); //read the pin value as a float
+var analogValue = analogPin1.read(); 
 
-var analogPin0 = new m.Aio(1); //setup access analog inpuput pin 0
 
-var analogValueFloat = analogPin0.readFloat(); //read the pin value as a float
 
 var groveSensor = require('jsupm_grove'); 
 
 var temp = new groveSensor.GroveTemp(0);
-var luz= new groveSensor.GroveLight(2);
-
-var ipAddress = '192.168.1.67'; 
 
 
-  function luz(){
-      
-      alert(luz.value());
-      
-    }
-        
+//Content for enable IoT
+var dgram = require('dgram');
+var client = dgram.createSocket('udp4');
 
+// UDP Options
+var options = {
+    host : '127.0.0.1',
+    port : 41234
+};
 
-
-function startServer()
+var countmsg =0;
+//Intel Enable Iot Send Observation Info.
+function sendObservation(name, value, on)
 {
-	console.log("Inicializando servidor web...");
-
-	var http = require('http');
-	http.createServer(function (req, res) {
-		  if(req.url == "/sensor") {
-            var analogValue = analogPin0.read(); //read the value of the analog pin
-			res.write(""+temp.value()+"ºC   ------  "+luz.value()+" Valor de luz "+analogValue+" Valor Humedad");
-            
-			res.end();
-		  }else{
-			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-			res.write("<!DOCTYPE html><html><body>");
-			res.write("<script>\n");
-			res.write("setInterval(function()	{\n");
-			res.write("	var xmlhttp=new XMLHttpRequest();		\n");	
-			res.write("	xmlhttp.onreadystatechange=function()	{\n");
-			res.write("		if (xmlhttp.readyState==4 && xmlhttp.status==200)	{\n");
-			res.write("			document.getElementById(\"termometro\").innerHTML= \"Temperatura: \"+xmlhttp.responseText + \"\";\n");
-			res.write("		}\n");
-			res.write("	}\n");
-			res.write("	xmlhttp.open(\"GET\",\"/sensor\",true);\n");
-			res.write("	xmlhttp.send();\n");
-			res.write("}, 100);	\n");
-			res.write("</script>	\n");	
-			res.write("<h1>Datos Web</h1>");
-			res.write("</p><p></p>");
-			res.write("<p><div id='termometro'></div>");
-            res.write('<FORM NAME="Formulario1"><INPUT TYPE="Button" NAME="Boton1" VALUE="Prueba" onClick="luz()"></FORM>');  
-			res.write("</body></html>");
-			res.end();
-
-		}
-		console.log("Request: " + req.url);  		
-	}).listen(1337, ipAddress);
+    var msg = JSON.stringify
+    ({
+        n: name,
+        v: value,
+        on: on
+    });
+    
+    var sentMsg = new Buffer(msg);
+    countmsg = countmsg + 1;
+    client.send(sentMsg, 0, sentMsg.length, options.port, options.host);
 }
 
-startServer();
+measureTemperature();
+medirHumedad();
+
+function measureTemperature()
+{
+    var Temperatura = temp.value();
+    sendObservation("temp", Temperatura, new Date().getTime());
+    console.log(Temperatura);
+    setTimeout(measureTemperature, 10000);
+}
+    
+function medirHumedad()
+{
+    var Humedad=analogValue;
+    sendObservation("hume", Humedad, new Date().getTime());
+    console.log("Humedad: "+Humedad);
+    setTimeout(medirHumedad, 10000);
+}
